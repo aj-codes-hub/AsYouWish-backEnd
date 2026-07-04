01304a1cae8d91216cd66b6da098f66ec1875709
aj-codes-hub <aalijjaan@gmail.com>
2026-07-04 23:56:33 +0500

    nothing

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,5 @@
 // src/controllers/adminController.js
-const User = require('../models/User');
+const User = require('../models/user');
 const Product = require('../models/Product');
 const Order = require('../models/Order');
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
-const User = require('../models/User');
-const { generateToken } = require('../jwt');
+const User = require("../models/user");
+const { generateToken } = require("../jwt");
 
 // Register
 const register = async (req, res) => {
