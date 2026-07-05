6fe6bebfcbe30f95d5755c85a7042e37ea9b1c7c
aj-codes-hub <aalijjaan@gmail.com>
2026-07-05 14:58:28 +0500

    fixed all errors

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,5 @@
 // src/controllers/adminController.js
-const User = require('../models/user');
+const User = require('../models/User');
 const Product = require('../models/Product');
 const Order = require('../models/Order');
 
